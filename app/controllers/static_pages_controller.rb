a13666879ff15fafa1ec545231dd303e51b11c1f
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-06-03 19:59:15 +0700

    Lession

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
+    @courses = Course.order("created_at desc").limit(6)
   end
 
   def help; end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   scope "(:locale)", locale: /vi|en/ do
-    root "courses#index"
+    root "static_pages#home" 
+    get "about", to: "static_pages#about"
+    get "contact", to: "static_pages#contact"
     resources :courses
     resources :lessons, except: :index
     resources :homeworks
